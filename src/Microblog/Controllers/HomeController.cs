c498e5b9a2cfbed7d3acc174f15f0c4ce69c991d
Jeroen V <j.vpersonal@hotmail.com>
2016-09-28 22:24:28 +0200

    - Had to recreate the project to get scaffolding to work (current limitation of ASP Core). - Cleaned up the generated project files. - Added index and login/registration stuff.

--- src/Microblog/Controllers/HomeController.cs
+++ src/Microblog/Controllers/HomeController.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace Microblog.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
         {
             return View();
         }
---
+++ src/Microblog/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Microblog.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
